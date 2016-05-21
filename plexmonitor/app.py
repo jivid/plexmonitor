713f6986d2da7d9ccda3637090285f3e4cd372de
Divij Rajkumar <drajkuma1@gmail.com>
2016-05-21 15:45:47 +0100

    Add setup.py script

---
+++ plexmonitor/app.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+from functools import partial
+
+from sparts.vservice import VService
+from sparts.compat import captureWarnings
+
+from plexmonitor.tasks.email_task import EmailTask
+
+
+class PlexMonitorService(VService):
+    TASKS = [
+        EmailTask,
+    ]
+
+    def initLogging(self) -> None:
+        fmt = "{asctime} [{levelname}] [{name}] {message}"
+        config_logging = partial(logging.basicConfig,
+                                 format=fmt,
+                                 level=self.loglevel,
+                                 style='{')
+        if self.logfile:
+            config_logging(filename=self.logfile)
+        else:
+            config_logging(stream=sys.stderr)
+
+        captureWarnings(True)
+
+
+if __name__ == '__main__':
+    PlexMonitorService.initFromCLI()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plexmonitor',
+    version='0.0',
+    packages=[
+        'plexmonitor',
+        'plexmonitor.settings',
+        'plexmonitor.lib',
+        'plexmonitor.tasks',
+    ],
+    entry_points={
+        'console_scripts': [
+            'plexmonitor = plexmonitor.app',
+        ],
+    },
+)

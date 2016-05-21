4171b0cc2f63c233a7c4150e3bc1c341e167e45f
Divij Rajkumar <drajkuma1@gmail.com>
2016-05-21 15:55:24 +0100

    Rename github settings file

--- plexmonitor/settings/__init__.py
+++ plexmonitor/settings/__init__.py
@@ -5,7 +5,7 @@ settings_path = os.getenv('PLEXMONITOR_SETTINGS')
 if settings_path:
     settings = importlib.import_module(settings_path)
 else:
-    settings = importlib.import_module('plexmonitor.settings.settings_github')
+    settings = importlib.import_module('plexmonitor.settings.github')
 
 globals().update(settings.__dict__)
 

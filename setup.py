20581911ebf4c9c394a7c6b98abf1202da63179d
Divij Rajkumar <drajkuma1@gmail.com>
2016-05-22 11:19:06 +0100

    Find packages

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='plexmonitor',
     version='0.0',
-    packages=[
-        'plexmonitor',
-        'plexmonitor.settings',
-        'plexmonitor.lib',
-        'plexmonitor.tasks',
-    ],
+    packages=find_packages('.'),
     entry_points={
         'console_scripts': [
             'plexmonitor = plexmonitor.app:main',
